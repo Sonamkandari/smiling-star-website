269821b9ae324cb0d7de61fcb05bef144d6989c8
Sonam Kandari <sonamkandari2004@gmail.com>
2026-02-16 18:23:05 +0530

    update base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
  export default defineConfig({
-  base: '/ smiling-stars-website/', //  Add this line
+  base: '/ smiling-star-website/', //  Add this line
   plugins: [react()],
 })
