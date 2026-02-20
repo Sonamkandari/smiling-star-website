cfd311bf104acb2841faeac6c909d81a1035cf4c
Sonam Kandari <sonamkandari2004@gmail.com>
2026-02-20 14:55:21 +0530

    updated

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -11,11 +11,14 @@ function Hero() {
       <img src={profile_img} alt='' />
       
       {/* main heading with name and title */}
-      <h1><span>Building Technology That Creates Social Impact,</span> Technology for Social Cause</h1>
+      <h1><span>Technology That Drives Sustainable Development</span> Technology for Social Cause</h1>
       
       {/* brief description */}
       <p>
-      We design and deliver technology solutions that empower communities and support meaningful social causes.
+        Smiling Star Private Limited is a technology‑driven social impact organization focused on creating Tech for Good solutions that address real‑world challenges across education, sustainability, inclusion, and community development.
+We partner with NGOs, educational institutions, social enterprises, and impact‑led organizations to design and deliver digital platforms, software systems, and data‑driven tools that advance sustainable development and long‑term social value.
+Our work directly supports global goals such as Quality Education (SDG 4), Industry, Innovation & Infrastructure (SDG 9), Reduced Inequalities (SDG 10), Sustainable Cities & Communities (SDG 11), and Climate Action (SDG 13).
+
       </p>
       
       <div className='hero-action'>
